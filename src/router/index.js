9cbe98863c23a54f8eebce8483a8681785cd0b15
Maherdika Erlambang <memerlambang@gmail.com>
2020-10-26 12:44:39 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+//use Vue Router in Vue JS
+Vue.use(Router);
+
+//import pages
+import AuthIndex from '@/views/auth/Index'
+import DahsboardIndex from '@/views/dashboard/Index'
+import HomeIndex from '@/views/home/Index'
+
+const routes = [
+      {
+        path: '/',
+        name: 'home',
+        component: HomeIndex
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: AuthIndex
+      },
+      {
+        path: '/dashoard',
+        name: 'dashoard',
+        component: DahsboardIndex
+      },
+]
+
+const router = new Router ({
+    routes:routes,
+    linkActiveClass: 'active'
+})
+
+export default router
---
+++ src/views/home/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <div class="jumbotron">
+            <div class="container">
+            <h1 class="display-3">TES BIDANG II - PT NINDYA KARYA (PERSERO)</h1>
+            <p>Aplikasi Laporan Harian Produksi</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Selengkapnya &raquo;</a></p>
+            </div>
+        </div>        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
